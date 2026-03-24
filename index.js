9adc08e952b14ab84d4cecba73d3e53443bdd43c
Royer Lavayen <ryr35472@gmail.com>
2026-03-24 16:04:30 -0500

    inicio backend rrhh

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    ok: true,
+    message: "Backend RRHH funcionando"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
